ed9ffb9bd34c333ff172f7b30afe0536c2ef4ccf
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-02-05 22:34:48 +0200

    Update unzip.cmd

--- unzip.cmd
+++ unzip.cmd
@@ -1,20 +1,22 @@
 @echo off
 title Y4hL/unzip
 
+:: Gets Files Path ::
 set file_dir=%~dp0
 
-set notcalledaglobalvariable=%random%
+:: Creates random number for folder name ::
+set add_dir=%random%
 
 :: Turns on powershell scripts for current user ::
 powershell.exe Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force;
 
+:: Checks whether or not arguments were given ::
 IF [%1] == [] (
 	echo Format:
 	echo.
 	echo path\unzip.cmd zip_file_path destination
 	echo.
-	echo Press any button to continue
-	pause >nul
+	timeout /t 10
 	goto :eof
 )
 
@@ -23,28 +25,29 @@ IF [%2] == [] (
 	echo.
 	echo path\unzip.cmd zip_file_path destination
 	echo.
-	echo Press any button to continue
-	pause >nul
+	timeout /t 10
 	goto :eof
 )
 
+:: Gets given arguments ::
 set zip=%1
 set dest=%2
 
 if not exist "%dest%" md "%dest%"
 
 :: Creates Temporary Directory ::
-md %file_dir%\%notcalledaglobalvariable% >nul
+md %file_dir%\%add_dir% >nul
 
-:: Writes code into %notcalledaglobalvariable%\unzipper.ps1 ::
-echo expand-archive -path '%zip%' -destinationpath '%dest%' -Force > %file_dir%\%notcalledaglobalvariable%\unzipper.ps1
+:: Writes code into %add_dir%\unzipper.ps1 ::
+echo expand-archive -path '%zip%' -destinationpath '%dest%' -Force > %file_dir%\%add_dir%\unzipper.ps1
 
 :: Runs Unzipper ::
-powershell.exe -File %file_dir%\%notcalledaglobalvariable%\unzipper.ps1
+powershell.exe -File %file_dir%\%add_dir%\unzipper.ps1
 
 :: Deletes Temporary Files ::
-del /F /Q %file_dir%\%notcalledaglobalvariable%\unzipper.ps1
+del /F /Q %file_dir%\%add_dir%\unzipper.ps1
 
-rd /s /q %file_dir%\%notcalledaglobalvariable%
+rd /s /q %file_dir%\%add_dir%
 
+:: End of File ::
 :eof
