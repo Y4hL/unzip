19f77d40b6e0be1f4cbdcb4aa7c04ba8ae47993c
Y4hL <43219534+Y4hL@users.noreply.github.com>
2019-08-11 11:12:52 +0300

    Made Code alot simpler

--- unzip.cmd
+++ unzip.cmd
@@ -40,19 +40,8 @@ set unzip_dest=%unzip_dest:"=%
 
 if not exist "%unzip_dest%" md "%unzip_dest%"
 
-:: Creates Temporary Directory ::
-md "%~dp0\%unzip_add_dir%" >nul
-
 :: Writes code into %add_dir%\unzipper.ps1 ::
-echo expand-archive -path '%unzip_zip%' -destinationpath '%unzip_dest%' -Force > "%~dp0\%unzip_add_dir%\unzipper.ps1"
-
-:: Runs Unzipper ::
-powershell.exe -File "%~dp0\%unzip_add_dir%\unzipper.ps1" >nul
-
-:: Deletes Temporary Files ::
-del /F /Q "%~dp0\%unzip_add_dir%\unzipper.ps1" >nul
-
-rd /s /q "%~dp0\%unzip_add_dir%" >nul
+powershell.exe -Command "expand-archive -path '%unzip_zip%' -destinationpath '%unzip_dest%' -Force"
 
 :: End of File ::
 :eof
